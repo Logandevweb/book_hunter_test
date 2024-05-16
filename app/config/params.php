2af9bd25c3d7f3a4a444e3371a5efcc507cbc05a
Logdevweb <logandejond@gmail.com>
2024-05-16 13:49:24 +0200

    structure mvc initiale

---
+++ app/config/params.php
@@ -0,0 +1,8 @@
+<?php
+// params
+
+// paramètres de connexion à la db
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'book_hunter_2024');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
